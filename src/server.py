4a79a3e6f795c499c8b80b5f36e3f9ae26844f57
Hugh <11092330+howhelan@users.noreply.github.com>
2021-06-30 16:55:32 +0100

    fix: bind on 0.0.0.0

--- src/server.py
+++ src/server.py
@@ -47,6 +47,6 @@ def redact_relay():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='localhost', port='8080')
+    app.run(debug=True, host='0.0.0.0', port='8080')
 
-app.run(host='localhost', port='8080')
+app.run(host='0.0.0.0', port='8080')
