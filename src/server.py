e807dd8c347317ac1462557f67ca7b1396e2a9c9
Hugh <11092330+howhelan@users.noreply.github.com>
2021-07-09 16:16:09 +0100

    feat: use client cert as user ID

--- src/server.py
+++ src/server.py
@@ -5,6 +5,8 @@ import json
 from flask import Flask, request, jsonify
 from flask_mongoengine import MongoEngine
 import os
+from OpenSSL import crypto
+from hashlib import sha256
 
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
@@ -23,34 +25,48 @@ class FeedPost(db.Document):
     timestamp = db.DateTimeField()
 
 
-@app.route("/feed/<user_id>", methods=['GET'])
-def index(user_id):
-    entries = FeedPost.objects(userId=user_id).order_by('-timestamp')
-    response_entries = []
-    for entry in entries:
-        response_entries.append({
-            "userId": entry.userId,
-            "contentReference": entry.contentReference,
-            "createdOn": entry.timestamp.isoformat()
-        })
-    return jsonify(response_entries), 200
+@app.route("/feed", methods=['GET'])
+def index():
+    user_cert = request.headers.get("X-Client-Cert")
+    if user_cert:
+        cert_object = crypto.load_certificate(crypto.FILETYPE_PEM, user_cert)
+        pub_key_object = cert_object.get_pubkey()
+        pub_key_string = crypto.dump_publickey(crypto.FILETYPE_PEM, pub_key_object)
+        user_id = sha256(pub_key_string).hexdigest()
+
+        entries = FeedPost.objects(userId=user_id).order_by('-timestamp')
+        response_entries = []
+        for entry in entries:
+            response_entries.append({
+                "userId": entry.userId,
+                "contentReference": entry.contentReference,
+                "createdOn": entry.timestamp.isoformat()
+            })
+        return jsonify(response_entries), 200
+    else:
+        return {}, 401
 
 
 @app.route("/redact/relay", methods=['POST'])
 def redact_relay():
-    FeedPost.objects.insert(FeedPost(
-        userId=request.json['userId'],
-        contentReference=request.json['path'],
-        timestamp=datetime.datetime.now())
-    )
+    user_cert = request.headers.get("X-Client-Cert")
+    if user_cert:
+        crtObj = crypto.load_certificate(crypto.FILETYPE_PEM, user_cert)
+        pubKeyObject = crtObj.get_pubkey()
+        pubKeyString = crypto.dump_publickey(crypto.FILETYPE_PEM, pubKeyObject)
+        userId = sha256(pubKeyString).hexdigest()
 
-    headers=[]
-    for h in request.headers.items():
-        headers.append({"h": h})
-    return jsonify(headers), 200
+        FeedPost.objects.insert(FeedPost(
+            userId=userId,
+            contentReference=request.json['path'],
+            timestamp=datetime.datetime.now())
+        )
+        return {}, 200
+    else:
+        return {}, 401
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port='8080')
+    app.run(debug=True, host='0.0.0.0', port='5000')
 
-app.run(host='0.0.0.0', port='8080')
+app.run(host='0.0.0.0', port='5000')
